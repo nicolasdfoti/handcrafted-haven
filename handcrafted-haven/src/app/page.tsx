ab1aeee06b586831a7c6cdcb9ee396c2fc57707d
Ezequiel Gimenez <eze.gimenez92@gmail.com>
2025-09-22 21:41:15 -0300

    Add hero image, titles, CTA buttons, change favicon and site name.

--- handcrafted-haven/src/app/page.tsx
+++ handcrafted-haven/src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import { Footer } from "./ui/footer.jsx";
 import { Header } from "./ui/header.jsx";
+import { Hero } from "./ui/hero.jsx";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
+      <Hero />
       <Footer />
     </div>
   );
