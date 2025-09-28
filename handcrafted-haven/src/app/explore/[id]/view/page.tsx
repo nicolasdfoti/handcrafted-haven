c1a8295fbe4036883ac4b6a7786a77eeca751953
Nicolás Foti <nicolasdfoti@gmail.com>
2025-09-28 19:19:05 -0300

    Added detail page

--- handcrafted-haven/src/app/explore/[id]/view/page.tsx
+++ handcrafted-haven/src/app/explore/[id]/view/page.tsx
@@ -1,28 +1,65 @@
+import { Header } from '@/app/ui/header.jsx';
+import { Footer } from "@/app/ui/footer";
+import { pool } from '@/app/lib/db';
+import { redirect } from "next/navigation";
+import styles from "@/app/styles/page.module.css"
 
-export default function Page() {
-  return <div>Hello</div>
-}
-
-//import { fetchProductById  } from "@/app/lib/data";
-
-/*
-import { notFound } from "next/navigation";
-
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const id = params.id;
-  const [invoice, customers] = await Promise.all([
-    fetchProductById(id),
-
-  ]);
-
-  if (!invoice) {
-    notFound();
+type DetailPageProps = {
+  params: {
+    id: string;
   }
-  return (
-    <main>
-
-    </main>
-  );
 }
-*/
+
+export default async function Page({ params }: DetailPageProps) {
+
+  const { id } = await params;
+  const res = await pool.query("SELECT * FROM account WHERE account_id = $1", [id]);
+  // const resProducts = await pool.query(
+  //   "SELECT * FROM products WHERE account_id = $1",
+  //   [id]
+  // );
+  // const products = resProducts.rows;
+
+  if (res.rows.length === 0) {
+    redirect( 'not-found.tsx')
+  }
+
+  const account = res.rows[0];
+
+  return (
+    <div>
+      <Header />
+
+      <div>
+        <div className={styles.detail}>
+          <h1>{account.account_firstname} {account.account_lastname}</h1>
+        </div>
+
+        <div className={styles.contact}>
+          <p>Contact info:</p>
+          <p>Email: {account.account_email}</p>
+        </div>
+      </div>
+
+      <h2>Products:</h2>
+
+      {/* <div className={styles.cards_container}>
+        {products.length === 0 && <p>No products yet.</p>}
+
+        {products.map((product) => (
+          <div key={product.product_id} className={styles.card}>
+            <section className={styles.card_title}>
+              <h2>{product.product_name}</h2>
+            </section>
+            <section className={styles.card_body}>
+              <p>{product.product_description}</p>
+              <p>Price: ${product.product_price}</p>
+            </section>
+          </div>
+        ))}
+      </div> */}
+
+      <Footer />
+    </div>
+  )
+}
