d5b697a3328335099216503da5818f6c12273c0b
Benny7412 <bennyklaus@gmail.com>
2025-09-30 20:52:36 -0500

    ADDED more structure and product as a definition

--- handcrafted-haven/src/app/explore/page.tsx
+++ handcrafted-haven/src/app/explore/page.tsx
@@ -1,6 +1,11 @@
 import { Footer } from "@/app/ui/footer.jsx";
 import { Header } from "@/app/ui/header.jsx";
 import styles from "../styles/page.module.css";
+import Image from "next/image";
+import { Product } from "@/app/lib/definitions";
+//import { fetchProductList } from "@/app/lib/data"
+
+//const productList = await fetchProductList();
 
 export default function Products() {
   return (
@@ -11,7 +16,24 @@ export default function Products() {
           <h1>hi</h1>
         </div>
         <div className={`${styles["marketplace__dynamic-content"]}`}>
-          <div className={`${styles["marketplace__products"]}`}></div>
+          <div className={`${styles["marketplace__products"]}`}>
+            <div className={`${styles["marketplace__product-card"]}`}>
+              <div className={`${styles["image-wrapper"]}`}>
+                {/*
+                <Image 
+                src={}//fetched product image
+                />*/}
+              </div>
+              <h2>Product Title</h2>
+
+              <a
+                className={`${styles["marketplace__listing-button"]}`}
+                href="#"
+              >
+                View Listing
+              </a>
+            </div>
+          </div>
           <div className={`${styles["marketplace__sidebar"]}`}>
             <h2>hi</h2>
           </div>
---
+++ handcrafted-haven/src/app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type Product = {
+  product_id: string;
+  product_name: string;
+  product_price: GLfloat;
+  image_url: string;
+  date: Date;
+};
