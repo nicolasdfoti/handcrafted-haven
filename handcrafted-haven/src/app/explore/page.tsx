cbe092495b8e0907ac96f4601b485874ea1eb440
Benny7412 <bennyklaus@gmail.com>
2025-09-30 19:19:13 -0500

    adding basic structure for marketplace

--- handcrafted-haven/src/app/explore/page.tsx
+++ handcrafted-haven/src/app/explore/page.tsx
@@ -4,10 +4,22 @@ import styles from "../styles/page.module.css";
 
 export default function Products() {
   return (
-    <div className={styles.page}>
+    <div className={`${styles["page"]} ${styles["marketplace__page"]}`}>
       <Header />
-      <main className={`${styles["marketplace-container"]}`}>
-        <h1>hello</h1>
+      <main className={`${styles["marketplace__container"]}`}>
+        <div className={`${styles["marketplace__header"]}`}>
+          <h1>hi</h1>
+        </div>
+        <div className={`${styles["marketplace__dynamic-content"]}`}>
+          <div className={`${styles["marketplace__products"]}`}></div>
+          <div className={`${styles["marketplace__sidebar"]}`}>
+            <h2>hi</h2>
+          </div>
+          {/*
+          <div className={`${styles["marketplace__mobile-sidebar"]}`}>
+              <h2>hi</h2>
+          </div>*/}
+        </div>
       </main>
       <Footer />
     </div>
